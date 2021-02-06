71fe2a11e2f146314a78c088588a80b78ed1ad53
Elvis Franks Condori Arias <massivestar7@gmail.com>
2021-02-06 10:28:53 -0500

    Initial Config DataBinding

--- app/src/main/java/com/example/testapp210202/MainActivity.kt
+++ app/src/main/java/com/example/testapp210202/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.testapp210202
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.testapp210202.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        
         
     }
 }
