d2d0548c9bf44508d57561d841e02efaed6a7f8d
Elvis Franks Condori Arias <massiveStar7@gmail.com>
2021-02-08 11:24:27 -0500

    Add Button Functionality

--- app/src/main/java/com/example/testapp210202/MainActivity.kt
+++ app/src/main/java/com/example/testapp210202/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.testapp210202
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -9,14 +10,73 @@ import com.example.testapp210202.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        viewModel.resultado.observe(this, Observer { binding.resultadoText.text = it.toString()  })
+        viewModel.resultado.observe(this, Observer { binding.resultadoText.text = it.toString() })
+
+        binding.sumaButton.setOnClickListener {
+            if (validacionDatos()) {
+                viewModel.calcularResultado(
+                        binding.dato1Edit.text.toString().toDouble(),
+                        binding.dato2Edit.text.toString().toDouble(),
+                        "suma")
+            } else
+                Toast.makeText(this, "Ingrese datos validos", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.restaButton.setOnClickListener {
+            if (validacionDatos()) {
+                viewModel.calcularResultado(
+                        binding.dato1Edit.text.toString().toDouble(),
+                        binding.dato2Edit.text.toString().toDouble(),
+                        "resta")
+            } else
+                Toast.makeText(this, "Ingrese datos validos", Toast.LENGTH_SHORT).show()
+        }
+        binding.multiplicacionButton.setOnClickListener {
+            if (validacionDatos()) {
+                viewModel.calcularResultado(
+                        binding.dato1Edit.text.toString().toDouble(),
+                        binding.dato2Edit.text.toString().toDouble(),
+                        "multiplicacion")
+            } else
+                Toast.makeText(this, "Ingrese datos validos", Toast.LENGTH_SHORT).show()
+        }
+        binding.divisionButton.setOnClickListener {
+            if (validacionDatos()) {
+                if (validacionDivCero(binding.dato2Edit.text.toString().toDouble())) {
+                    viewModel.calcularResultado(
+                            binding.dato1Edit.text.toString().toDouble(),
+                            binding.dato2Edit.text.toString().toDouble(),
+                            "division")
+                } else
+                    Toast.makeText(this, "No se puede dividir por cero", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Ingrese datos validos", Toast.LENGTH_SHORT).show()
+        }
+        binding.limpiarButton.setOnClickListener {
+            binding.apply {
+                dato1Edit.text.clear()
+                dato2Edit.text.clear()
+                resultadoText.text = ""
+            }
+        }
+
+    }
+
+    private fun validacionDatos(): Boolean {
+        return binding.dato1Edit.text.toString().toDoubleOrNull() != null &&
+                binding.dato2Edit.text.toString().toDoubleOrNull() != null
+    }
+
+    private fun validacionDivCero(dividendo: Double): Boolean {
+        return dividendo != 0.0
     }
 }
--- app/src/main/java/com/example/testapp210202/MainViewModel.kt
+++ app/src/main/java/com/example/testapp210202/MainViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     private val _dato1 = MutableLiveData<Double>()
     private val _dato2 = MutableLiveData<Double>()
@@ -22,6 +22,16 @@ class MainViewModel: ViewModel() {
         get() = _dato2
     val resultado: LiveData<Double>
         get() = _resultado
+
+    fun calcularResultado(param1: Double, param2: Double, operacion: String) {
+
+        _resultado.value = when (operacion) {
+            "suma" -> param1.plus(param2)
+            "resta" -> param1.minus(param2)
+            "multiplicacion" -> param1.times(param2)
+            else -> param1.div(param2)
+        }
+    }
 
     override fun onCleared() {
         super.onCleared()
